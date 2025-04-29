b2e5f185770a144837e3b2434e8bc728625f0c85
Nathan <nathan@MacBook-Pro-de-Nathan.local>
2025-04-28 20:53:49 -0400

    Feat(master): Unit test Angular

--- src/test/java/com/nathan/duoc_usuarios/DuocUsuariosApplicationTests.java
+++ src/test/java/com/nathan/duoc_usuarios/DuocUsuariosApplicationTests.java
@@ -1,13 +1,9 @@
 package com.nathan.duoc_usuarios;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DuocUsuariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
